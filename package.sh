0c81d8f3f28ab7df9497c2f93bef15202c99310c
Jonathan 'NekoLover' Raffre <jonathan.raffre@nekolover.net>
2013-08-26 21:46:04 +0200

    Properly insert logstash at boot, correctly handle deletion of user w/ purge

--- package.sh
+++ package.sh
@@ -2,4 +2,4 @@
 VERSION=1.1.13
 cd ..
 wget http://logstash.objects.dreamhost.com/release/logstash-${VERSION}-monolithic.jar -O logstash-packaging/usr/share/logstash/logstash.jar
-fpm -n logstash -v $VERSION -a all -C logstash-packaging -m "<jonathan.raffre@nekolover.net>" --pre-install logstash-packaging/logstash.preinstall --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog.conf --config-files etc/default/logstash -d java-runtime -s dir etc usr var
+fpm -n logstash -v $VERSION -a all -C logstash-packaging -m "<jonathan.raffre@nekolover.net>" --before-install logstash-packaging/logstash.preinstall --after-install logstash-packaging/logstash.postinstall --before-remove logstash-packaging/logstash.preremove --after-remove logstash-packaging/logstash.postremove --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog.conf --config-files etc/default/logstash -d java-runtime -s dir etc usr var

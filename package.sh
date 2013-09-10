031d6910e78eb19b02fd05bd2e6aced56c555532
NekoLover <nekolover@nekolover.net>
2013-09-10 11:56:25 +0200

    updating logstash to 1.2.1

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.1.13
+VERSION=1.2.1
 cd ..
-wget http://logstash.objects.dreamhost.com/release/logstash-${VERSION}-monolithic.jar -O logstash-packaging/usr/share/logstash/logstash.jar
-fpm -n logstash -v $VERSION -a all -C logstash-packaging -m "<jonathan.raffre@nekolover.net>" --before-install logstash-packaging/logstash.preinstall --after-install logstash-packaging/logstash.postinstall --before-remove logstash-packaging/logstash.preremove --after-remove logstash-packaging/logstash.postremove --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog.conf --config-files etc/default/logstash -d java-runtime -s dir etc usr var
+wget http://logstash.objects.dreamhost.com/release/logstash-${VERSION}-flatjar.jar -O logstash-packaging/usr/share/logstash/logstash.jar
+fpm -n logstash -v $VERSION -a all -C logstash-packaging -m "<jonathan.raffre@nekolover.net>" --before-install logstash-packaging/logstash.preinstall --after-install logstash-packaging/logstash.postinstall --before-remove logstash-packaging/logstash.preremove --after-remove logstash-packaging/logstash.postremove --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/logstash.conf --config-files etc/default/logstash --config-files /etc/logstash/patterns/ipv6 -d java-runtime -s dir etc usr var
